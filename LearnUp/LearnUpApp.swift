b5bfc13333c6034b2ac894ba4038614f2cba5fce
Iann221 <vincentiannugroho@gmail.com>
2022-07-24 21:42:10 +0700

    Initial Commit

---
+++ LearnUp/LearnUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnUpApp.swift
+//  LearnUp
+//
+//  Created by Vincentius Ian Widi Nugroho on 24/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
